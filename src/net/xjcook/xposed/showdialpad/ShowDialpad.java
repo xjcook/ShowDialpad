be2e90bbb2cd3e9d641238ab2988548f60cff784
XjCook <xjcook@gmail.com>
2013-12-14 15:50:04 +0100

    Add support of AOSP Dialer

--- src/net/xjcook/xposed/showdialpad/ShowDialpad.java
+++ src/net/xjcook/xposed/showdialpad/ShowDialpad.java
@@ -3,25 +3,43 @@ package net.xjcook.xposed.showdialpad;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findMethodExact;
 
+import java.lang.reflect.Method;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class ShowDialpad implements IXposedHookLoadPackage {
 
+	static final String GOOGLE_DIALER = "com.google.android.dialer";
+	static final String AOSP_DIALER = "com.android.dialer";
+	
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		if (!lpparam.packageName.equals("com.google.android.dialer"))
-            return;
-
-		findAndHookMethod("com.android.dialer.DialtactsActivity", lpparam.classLoader, "onResume", new XC_MethodHook() {
-			@Override	
-    		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				final Object thisObject = param.thisObject;
-				findMethodExact(thisObject.getClass().getSuperclass(), "showDialpadFragment", boolean.class)
-						.invoke(thisObject, false);
-    		}
-		});
+		if (lpparam.packageName.equals(GOOGLE_DIALER)) {
+		
+			findAndHookMethod("com.android.dialer.DialtactsActivity", lpparam.classLoader, "onResume", new XC_MethodHook() {
+				@Override	
+	    		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					final Object thisObject = param.thisObject;
+					findMethodExact(thisObject.getClass().getSuperclass(), "showDialpadFragment", boolean.class)
+							.invoke(thisObject, false);
+	    		}
+			});
+		
+		} else if (lpparam.packageName.equals(AOSP_DIALER)) {
+			
+			findAndHookMethod("com.android.dialer.DialtactsActivity", lpparam.classLoader, "onResume", new XC_MethodHook() {
+				@Override	
+	    		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					final Object thisObject = param.thisObject;
+					findMethodExact(thisObject.getClass(), "showDialpadFragment", boolean.class)
+							.invoke(thisObject, false);
+	    		}
+			});
+			
+		}
 	}
 
 }
